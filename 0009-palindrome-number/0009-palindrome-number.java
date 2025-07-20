39e2fa543558d2a1f43df4255e7ba3bd403120b1
AAKASH KUMAR SINGH <aksrajput3453@gmail.com>
2025-07-21 00:22:01 +0530

    Time: 5 ms (66.92%), Space: 44.4 MB (26.11%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int sum =0 , d=0;
+        for (int i= x; i>0; i=i/10){
+             d=i%10;
+           sum=sum*10 + d;
+        }
+        if(sum==x)
+            return true ;
+        else 
+            return false ;
+        
+    }
+}
