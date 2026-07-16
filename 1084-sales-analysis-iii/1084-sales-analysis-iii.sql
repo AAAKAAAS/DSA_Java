ac1d67be9b4ad6ac11c40038b5abbb3a6763fc8f
AAKASH KUMAR SINGH <aksrajput3453@gmail.com>
2026-07-17 01:30:59 +0530

    Time: 1081 ms (94.28%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.product_id,p.product_name from Product p
+right join Sales S
+on p.product_id =s.product_id
+group by s.product_id
+having (max(sale_date) between '2019-01-01' and '2019-03-31') and (min(sale_date) between '2019-01-01' and '2019-03-31')
